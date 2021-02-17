9d64ad43ede481e6bf2fb6702c4846f5ebc00424
luis otavio <luotasss@gmail.com>
2021-02-17 15:15:11 -0300

    add main

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class Application(QWidget):
+    def __init__(self):
+        super(Application, self).__init__()
+        self.setWindowTitle("Calculator")
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Application()
+    sys.exit(app.exec())
